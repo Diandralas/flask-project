a1f302bb93b83e923c971c54211c330077f70b69
diandra <di.andrade90@gmail.com>
2020-01-09 09:19:21 -0300

    Create simple example with GET and POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  #import library
+from flask import Flask, render_template, redirect, request, url_for  #import library
 app = Flask(__name__) #start the app
 
 @app.route('/hello')    #create a new route
@@ -8,6 +8,16 @@ def hello():
 @app.route('/index') 
 def index():
     return render_template("index.html")
+
+@app.route('/login', methods= ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['user']
+        return redirect (url_for('index', name = user))
+    else:
+        user = request.args.get('user')
+        return redirect (url_for('index', name = user))
+
 
 if __name__ == "__main__":
     app.run(debug=True)   #exec the app
